7c06768c2a5167e0ec03fa2d8b2467f8cbde32af
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-08-31 23:13:40 -0300

    Change of home page

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import styles from './Home.module.css';
 function Home() {
     return (
         <>
-            <h1>Conteúdo da página home</h1>
+            <h1 className={styles.title}>Conteúdo da página home</h1>
         </> 
     )
 }
