1db390e4875d01ed388726c980217ad876b788be
Letícia França <99284224+LeticiaSFranca@users.noreply.github.com>
2023-09-04 21:27:48 -0300

    Page Home - Finish

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,13 +1,26 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
 
+import pickaxe from '../../assets/images/pickaxe.png'
+import sword from '../../assets/images/sword.png'
+import CraftAPI from '../../assets/images/creeper.png'
+import ax from '../../assets/images/ax.png'
+
 function Header() {
     return (
         <>
+
             <header className={styles.header}>
-                <Link to="/">Home</Link>
-                <Link to="/mobs">Lista de mobs</Link>
-                <Link to="/detalhes-mob">Detalhes</Link>
+                <logo>
+                    <Link to="/"><img src={CraftAPI} alt="logoCraftAPI" className={styles.logoCraftAPI} />CraftAPI</Link>
+                </logo>
+
+                <Link to="/"><img src={ax} alt="ax" className={styles.iconesHeader} />
+                   Home</Link>
+                <Link to="/mobs"><img src={sword} alt="sword" className={styles.iconesHeader} />
+                   Mob List</Link>
+                <Link to="/detalhes-mob"><img src={pickaxe} alt="pickaxe" className={styles.iconesHeader} />
+                   Details</Link>
             </header>
         </>
     )
