7abe1d8b9a32374aadd6d1d4b763f0d73a46854c
Letícia França <leticiasilvafranca17@gmail.com>
2023-09-05 08:10:44 -0300

    Create LinkedIn Button

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import styles from './Home.module.css';
 import github from '../../assets/images/Icons/github.png';
 import webIcon from '../../assets/images/Icons/web.png';
+import linkedin from "../../assets/images/Icons/linkedin.png";
 import leticiaPhoto from '../../assets/images/Developers/leticia.jpg';
 import arturPhoto from '../../assets/images/Developers/artur.png';
 
@@ -13,7 +14,7 @@ function Home() {
                         Olá! Somos Artur e Letícia, criadores deste projeto! Tudo bem para você? <br />
                         Este site foi desenvolvido com a intenção de realizar um trabalho escolar, e consiste na construção
                         de uma API e consumação dela em uma página da web. <br />
-                        Esperamos que você goste! :)
+                        Esperamos que você goste!
                     </p>
                 </div>
                 <div className={styles.all_cards}>
@@ -23,6 +24,7 @@ function Home() {
                             <p className={styles.names}>Artur Bomtempo</p>
                             <a href="https://github.com/ArturColen" target="_blank" rel="noopener noreferrer"><img src={github} alt="github" className={styles.card_icons} /></a>
                             <a href="https://arturbomtempo.vercel.app" target="_blank" rel="noopener noreferrer"><img src={webIcon} alt="Portfólio" className={styles.card_icons} /></a>
+                            <a href="https://www.linkedin.com/in/artur-bomtempo/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="LinkedIn" className={styles.card_icons} /></a>
                         </div>
                     </div>
                     <div className={styles.cards_content}>
@@ -31,6 +33,7 @@ function Home() {
                             <p className={styles.names}>Letícia França</p>
                             <a href="https://github.com/LeticiaSFranca" target="_blank" rel="noopener noreferrer"><img src={github} alt="github" className={styles.card_icons} /></a>
                             <a href="https://lsfranca.netlify.app/" target="_blank" rel="noopener noreferrer"><img src={webIcon} alt="Portfólio" className={styles.card_icons} /></a>
+                            <a href="https://www.linkedin.com/in/leticiasfranca/" target="_blank" rel="noopener noreferrer"><img src={linkedin} alt="LinkedIn" className={styles.card_icons} /></a>
                         </div>
                     </div>
                 </div>
