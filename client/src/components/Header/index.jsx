8add013333769f216389bf67b83097a9640cf903
Letícia França <99284224+LeticiaSFranca@users.noreply.github.com>
2023-09-10 20:15:44 -0300

    Updates from Home

--- client/src/components/Header/index.jsx
+++ client/src/components/Header/index.jsx
@@ -1,16 +1,16 @@
 import { Link } from 'react-router-dom';
 import styles from './Header.module.css';
-import block from '../../assets/images/HeaderIcons/block.png';
-import logo from '../../assets/images/logo.png';
-import sword from '../../assets/images/HeaderIcons/sword.webp';
-import diamond from '../../assets/images/HeaderIcons/diamond.webp';
+import home from '../../assets/images/HeaderIcons/home.png';
+import creeper from '../../assets/images/HeaderIcons/creeper.png';
+import sword from '../../assets/images/HeaderIcons/espada.png';
+import diamond from '../../assets/images/HeaderIcons/picareta.png';
 
 
 function Header() {
     return (
         <header className={styles.header}>
-            <Link to="/"><img src={block} alt="Imagem do bloco de terra" className={styles.header_icons} />Home</Link>
-            <Link to="/mobs"> <img src={logo} alt="Imagem do creeper" className={styles.header_icons} />Mobs</Link>
+            <Link to="/"><img src={home} alt="Imagem do bloco de terra" className={styles.header_icons} />Home</Link>
+            <Link to="/mobs"> <img src={creeper} alt="Imagem do creeper" className={styles.header_icons} />Mobs</Link>
             <Link to="/equipamentos"> <img src={sword} alt="Imagem da espada" className={styles.header_icons} />Equipamentos</Link>
             <Link to="/minerios"> <img src={diamond} alt="Imagem do minério de diamante" className={styles.header_icons} />Minérios</Link>
         </header>
