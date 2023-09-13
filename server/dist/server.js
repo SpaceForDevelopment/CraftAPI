eb216fcc0793fabfb8afde8b30079384dd859438
Artur Bomtempo Colen <arturbcolen@gmail.com>
2023-09-13 00:56:03 -0300

    docs: addition of the API documentation made with Swagger

--- server/dist/server.js
+++ server/dist/server.js
@@ -6,10 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_middleware_js_1 = require("./middlewares/cors-middleware.js");
 const index_js_1 = __importDefault(require("./routes/index.js"));
+const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
+const swagger_json_1 = __importDefault(require("./swagger.json"));
 const PORT = process.env.PORT || 3000;
 const app = (0, express_1.default)();
 (0, cors_middleware_js_1.configureCORS)(app);
 app.use('/', index_js_1.default);
+index_js_1.default.use('/documentation', swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swagger_json_1.default));
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,8 @@
 import express from 'express';
 import { configureCORS } from './middlewares/cors-middleware.js';
 import router from './routes/index.js';
+import swaggerUI from 'swagger-ui-express';
+import swaggerDocs from './swagger.json';
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -8,6 +10,8 @@ const app = express();
 configureCORS(app);
 
 app.use('/', router);
+
+router.use('/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
