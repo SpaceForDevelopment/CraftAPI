476af79d0c43c3ef4b4beb161ab5d05e1b2c6ab3
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-03-08 23:59:40 -0300

    chore: footer adjustments

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,10 +1,8 @@
 import styles from './Footer.module.css';
-import cotemig from '../../assets/images/Icons/cotemig.png';
 
 function Footer() {
     return (
         <footer className={styles.footer}>
-            <a href="https://www.cotemig.com.br/" target="_blank" rel="noopener noreferrer"><img src={cotemig} alt="Logo do Cotemig" className={styles.logo_cotemig} /></a> <br />
             Desenvolvido por Artur Bomtempo e Letícia França
         </footer>
     );
