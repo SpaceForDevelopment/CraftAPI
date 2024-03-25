a187ffb9e1552e5743cb9f264374e78b895aa4d2
Artur Bomtempo Colen <arturbcolen@gmail.com>
2024-03-24 23:50:43 -0300

    refactor(server): changes to the API structure

--- server/src/routes/index-router.ts
+++ server/src/routes/index-router.ts
@@ -1,12 +1,8 @@
 import express from 'express';
 import { indexController } from '../controllers/index-controller.js';
-import swaggerUI from 'swagger-ui-express';
-import swaggerDocs from '../swagger.json';
 
 const indexRouter = express.Router();
 
 indexRouter.get('/', indexController);
-
-indexRouter.use('/documentation', swaggerUI.serve, swaggerUI.setup(swaggerDocs));
 
 export { indexRouter };
